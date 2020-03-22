5bd4837b4ef47ce1192495d4e70fa5348f3e99a2
rajchunara <rajchunara777@gmail.com>
2020-03-22 18:27:38 -0400

    Updates

---
+++ precache-manifest.6e931118bd44fdb6f39821207534e6e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13a4f72fcc0dea7cd260e4dace5adad",
+    "url": "/react-ci-cd/index.html"
+  },
+  {
+    "revision": "adece0e75fa65fde2442",
+    "url": "/react-ci-cd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2e02c01b7edc48cce4c",
+    "url": "/react-ci-cd/static/js/2.d33fe9b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ci-cd/static/js/2.d33fe9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adece0e75fa65fde2442",
+    "url": "/react-ci-cd/static/js/main.f9088ef3.chunk.js"
+  },
+  {
+    "revision": "1806c10265382da3e005",
+    "url": "/react-ci-cd/static/js/runtime-main.5cc43896.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ci-cd/static/media/logo.5d5d9eef.svg"
+  }
+]);
